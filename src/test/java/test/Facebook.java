8657884a6253ce7356994200744b4b876034ed11
Harsha Vardhan <gvsharsha6316@gmail.com>
2022-02-09 02:10:11 -0500

    initial commit

---
+++ src/test/java/test/Facebook.java
@@ -0,0 +1,11 @@
+package test;
+
+import org.testng.annotations.Test;
+
+public class Facebook {
+
+    @Test
+    public void test1() {
+        System.out.println("this is testcase from facebook class!!");
+    }
+}
