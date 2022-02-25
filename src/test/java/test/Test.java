3fcf72d7aff1b223c795f630cfc6e0fc1ece3bc8
Harsha Vardhan <gvsharsha6316@gmail.com>
2022-02-25 06:58:59 -0500

    log4j

---
+++ src/test/java/test/Test.java
@@ -0,0 +1,15 @@
+package test;
+
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
+
+public class Test {
+    final static Logger l = Logger.getLogger(Test.class);
+    public static void main(String[] args) {
+
+        PropertyConfigurator.configure("./src/test/resources/log4j.properties");
+        l.info("harsha");
+        l.warn("harsha");
+        l.fatal("harsha");
+    }
+}
